706361d23aba6fab73b096dd92aefdeb48830ac2
Max Valek <valek.maxwell@gmail.com>
2023-05-12 18:24:00 -0500

    add basic user and tweet models

---
+++ SlideOutMenuSwiftUI/Model/Tweet.swift
@@ -0,0 +1,12 @@
+//
+//  Tweet.swift
+//  SlideOutMenuSwiftUI
+//
+//  Created by Max Valek on 5/12/23.
+//
+
+import Foundation
+
+struct Tweet: Identifiable {
+    var id = UUID()
+}
---
+++ SlideOutMenuSwiftUI/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  SlideOutMenuSwiftUI
+//
+//  Created by Max Valek on 5/12/23.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    var id = UUID()
+}
