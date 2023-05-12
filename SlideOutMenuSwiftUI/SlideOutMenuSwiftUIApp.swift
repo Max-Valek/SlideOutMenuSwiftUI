866422e7b7d5fd3381a1c0f18825c95f507892a5
Max Valek <valek.maxwell@gmail.com>
2023-05-12 13:28:56 -0500

    Initial Commit

---
+++ SlideOutMenuSwiftUI/SlideOutMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOutMenuSwiftUIApp.swift
+//  SlideOutMenuSwiftUI
+//
+//  Created by Max Valek on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SlideOutMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
