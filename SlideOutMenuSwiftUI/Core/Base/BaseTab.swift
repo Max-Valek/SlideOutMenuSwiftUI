405682d12d833e3540b8ba2240ab45aa57f0b72d
Max Valek <valek.maxwell@gmail.com>
2023-05-12 17:55:42 -0500

    extract base view tab enum

---
+++ SlideOutMenuSwiftUI/Core/Base/BaseTab.swift
@@ -0,0 +1,44 @@
+//
+//  BaseTab.swift
+//  SlideOutMenuSwiftUI
+//
+//  Created by Max Valek on 5/12/23.
+//
+
+import Foundation
+
+// MARK: - BaseTab enum
+
+enum BaseTab {
+    case home, search, notifications, messages
+}
+
+// title and image properties
+extension BaseTab {
+    
+    var title: String {
+        switch self {
+        case .home:
+            return "Home"
+        case .search:
+            return "Search"
+        case .notifications:
+            return "Notifications"
+        case .messages:
+            return "Messages"
+        }
+    }
+    
+    var image: String {
+        switch self {
+        case .home:
+            return "house"
+        case .search:
+            return "magnifyingglass"
+        case .notifications:
+            return "bell"
+        case .messages:
+            return "envelope"
+        }
+    }
+}
--- SlideOutMenuSwiftUI/Core/Base/BaseView.swift
+++ SlideOutMenuSwiftUI/Core/Base/BaseView.swift
@@ -7,41 +7,6 @@
 
 import SwiftUI
 
-// MARK: - BaseTab enum
-
-enum BaseTab {
-    case home, search, notifications, messages
-}
-
-// title and image properties
-extension BaseTab {
-    
-    var title: String {
-        switch self {
-        case .home:
-            return "Home"
-        case .search:
-            return "Search"
-        case .notifications:
-            return "Notifications"
-        case .messages:
-            return "Messages"
-        }
-    }
-    
-    var image: String {
-        switch self {
-        case .home:
-            return "house"
-        case .search:
-            return "magnifyingglass"
-        case .notifications:
-            return "bell"
-        case .messages:
-            return "envelope"
-        }
-    }
-}
 
 // MARK: - View
 
