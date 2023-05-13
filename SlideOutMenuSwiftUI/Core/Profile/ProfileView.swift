afd3ac291f9d12da46b2a49f855bd193dbefa074
Max Valek <valek.maxwell@gmail.com>
2023-05-12 19:09:20 -0500

    add header image, profile image, and edit profile button to profile

--- SlideOutMenuSwiftUI/Core/Profile/ProfileView.swift
+++ SlideOutMenuSwiftUI/Core/Profile/ProfileView.swift
@@ -18,6 +18,52 @@ struct ProfileView: View {
             Color.theme.black
                 .ignoresSafeArea()
             
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    
+                    // header photo
+                    VStack {
+                        Image(user.headerImage ?? "defaultHeader")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 100)
+                    }
+                    
+                    HStack(alignment: .center) {
+                        Image(user.profileImage ?? "default")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 70, height: 70)
+                            .clipShape(Circle())
+                            .padding(3)
+                            .background(Color.theme.black, in: Circle())
+                        
+                        Spacer()
+                        
+                        Button {
+                            // edit profile pressed
+                        } label: {
+                            Text("Edit Profile")
+                                .font(.subheadline)
+                                .foregroundColor(Color.theme.text)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 12)
+                                .background(Color.theme.black)
+                                .clipShape(Capsule())
+                                .padding(1)
+                                .background(Color.theme.darkGray, in: Capsule())
+                        }
+                        .padding(.top, 24)
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                }
+                
+            }
+            .ignoresSafeArea()
+            
             VStack(spacing: 0) {
                 
                 Text(user.displayName)
