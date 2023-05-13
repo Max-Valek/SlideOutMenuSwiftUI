b6ff088e424430a33a56ae7ffd42b72d5cb9e81c
Max Valek <valek.maxwell@gmail.com>
2023-05-12 20:07:19 -0500

    refactor profile view

--- SlideOutMenuSwiftUI/Core/Profile/ProfileView.swift
+++ SlideOutMenuSwiftUI/Core/Profile/ProfileView.swift
@@ -17,136 +17,34 @@ struct ProfileView: View {
     var body: some View {
         
         ZStack {
-            
             Color.theme.black
                 .ignoresSafeArea()
             
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    
                     // header photo
-                    VStack {
-                        Image(user.headerImage ?? "defaultHeader")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 100)
-                    }
-                    
+                    headerImage
                     // profile pic and edit profile button
-                    HStack(alignment: .center) {
-                        Image(user.profileImage ?? "default")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 70, height: 70)
-                            .clipShape(Circle())
-                            .padding(3)
-                            .background(Color.theme.black, in: Circle())
-                        
-                        Spacer()
-                        
-                        Button {
-                            // edit profile pressed
-                        } label: {
-                            Text("Edit Profile")
-                                .font(.subheadline)
-                                .foregroundColor(Color.theme.text)
-                                .padding(.vertical, 4)
-                                .padding(.horizontal, 12)
-                                .background(Color.theme.black)
-                                .clipShape(Capsule())
-                                .padding(2)
-                                .background(Color.theme.twitterBlack, in: Capsule())
-                        }
-                        .padding(.top, 24)
-                    }
-                    .padding(.horizontal)
-                    
+                    photoAndEditProfile
                     // display name and username
-                    VStack(alignment: .leading, spacing: 4) {
-                        // display name
-                        HStack {
-                            Text(user.displayName)
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            
-                            if user.isVerified {
-                                Image(systemName: "checkmark.seal.fill")
-                                    .font(.subheadline)
-                                    .foregroundColor(Color.theme.blue)
-                            }
-                        }
-                        
-                        Text("@\(user.username)")
-                            .font(.subheadline)
-                            .foregroundColor(Color.theme.lightGray)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    .padding(.bottom, 8)
-                    
+                    nameAndUsername
                     // user bio
-                    if let bio = user.bio {
-                        HStack {
-                            Text(bio)
-                                .font(.subheadline)
-                            
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                        .padding(.bottom, 8)
-                    }
-                    
+                    userBio
                     // following, followers
-                    HStack(spacing: 12) {
-                        
-                        HStack(spacing: 4) {
-                            Text("\(user.following.count)")
-                            
-                            Text("Following")
-                                .foregroundColor(Color.theme.lightGray)
-                        }
-                        
-                        HStack(spacing: 4) {
-                            Text("\(user.followers.count)")
-                            
-                            Text("Followers")
-                                .foregroundColor(Color.theme.lightGray)
-                        }
-                        
-                        Spacer()
-                    }
-                    .font(.subheadline)
-                    .padding(.horizontal)
-                    .padding(.bottom, 8)
-                    
+                    followingAndFollowers
                     // tweets, replies, media, likes tabs
                     tabs
-                    
                     Divider()
-                    
                     // content
-                    switch currentTab {
-                    case .tweets:
-                        Text("Tweets")
-                    case .replies:
-                        Text("Replies")
-                    case .media:
-                        Text("Media")
-                    case .likes:
-                        Text("Likes")
-                    }
-                    
+                    content
                     Spacer()
                 }
-                
-                
             }
             .ignoresSafeArea()
-            
         }
         .foregroundColor(Color.theme.text)
         .navigationBarBackButtonHidden(true)
+        // back and search buttons
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Button {
@@ -188,6 +86,108 @@ struct ProfileView_Previews: PreviewProvider {
 }
 
 extension ProfileView {
+    // header image
+    private var headerImage: some View {
+        VStack {
+            Image(user.headerImage ?? "defaultHeader")
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: .infinity)
+                .frame(height: 100)
+        }
+    }
+    // user profile photo and edit profile button
+    private var photoAndEditProfile: some View {
+        HStack(alignment: .center) {
+            Image(user.profileImage ?? "default")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 70, height: 70)
+                .clipShape(Circle())
+                .padding(3)
+                .background(Color.theme.black, in: Circle())
+            
+            Spacer()
+            
+            Button {
+                // edit profile pressed
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline)
+                    .foregroundColor(Color.theme.text)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 12)
+                    .background(Color.theme.black)
+                    .clipShape(Capsule())
+                    .padding(2)
+                    .background(Color.theme.twitterBlack, in: Capsule())
+            }
+            .padding(.top, 24)
+        }
+        .padding(.horizontal)
+    }
+    // name and username
+    private var nameAndUsername: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            // display name
+            HStack {
+                Text(user.displayName)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+                if user.isVerified {
+                    Image(systemName: "checkmark.seal.fill")
+                        .font(.subheadline)
+                        .foregroundColor(Color.theme.blue)
+                }
+            }
+            
+            Text("@\(user.username)")
+                .font(.subheadline)
+                .foregroundColor(Color.theme.lightGray)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal)
+        .padding(.bottom, 8)
+    }
+    // bio
+    @ViewBuilder
+    private var userBio: some View {
+        if let bio = user.bio {
+            HStack {
+                Text(bio)
+                    .font(.subheadline)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 8)
+        }
+    }
+    // following/followers
+    private var followingAndFollowers: some View {
+        HStack(spacing: 12) {
+            
+            HStack(spacing: 4) {
+                Text("\(user.following.count)")
+                
+                Text("Following")
+                    .foregroundColor(Color.theme.lightGray)
+            }
+            
+            HStack(spacing: 4) {
+                Text("\(user.followers.count)")
+                
+                Text("Followers")
+                    .foregroundColor(Color.theme.lightGray)
+            }
+            
+            Spacer()
+        }
+        .font(.subheadline)
+        .padding(.horizontal)
+        .padding(.bottom, 8)
+    }
     // tweets, replies, media, likes
     private var tabs: some View {
         HStack {
@@ -220,5 +220,19 @@ extension ProfileView {
             .fontWeight(.semibold)
         }
         
+    }
+    // content
+    @ViewBuilder
+    private var content: some View {
+        switch currentTab {
+        case .tweets:
+            Text("Tweets")
+        case .replies:
+            Text("Replies")
+        case .media:
+            Text("Media")
+        case .likes:
+            Text("Likes")
+        }
     }
 }
