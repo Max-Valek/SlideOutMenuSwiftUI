a025a0d90a89583653689148d679aa157f456b36
Max Valek <valek.maxwell@gmail.com>
2023-05-15 15:28:25 -0500

    add conversation view header

--- SlideOutMenuSwiftUI/Core/Messages/ConversationView.swift
+++ SlideOutMenuSwiftUI/Core/Messages/ConversationView.swift
@@ -9,15 +9,64 @@ import SwiftUI
 
 struct ConversationView: View {
     
+    @Environment(\.dismiss) var dismiss
+    
     let conversation: Conversation
     
     var body: some View {
-        Text(conversation.users[0].displayName)
+        ZStack {
+            Color.theme.black.ignoresSafeArea()
+            
+            VStack {
+                // header
+                header
+                
+                Spacer()
+            }
+        }
+        .foregroundColor(Color.theme.text)
+        // make custom header instead
+        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
 struct ConversationView_Previews: PreviewProvider {
     static var previews: some View {
         ConversationView(conversation: Conversation.sampleConversations[0])
+    }
+}
+
+extension ConversationView {
+    // header
+    private var header: some View {
+        HStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "arrow.left")
+                    .font(.title3)
+            }
+            Spacer()
+            VStack {
+                Image(conversation.users[0].profileImage ?? "default")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
+                
+                Text(conversation.users[0].displayName)
+                    .font(.headline)
+            }
+            Spacer()
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "info.circle")
+                    .font(.title3)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(Color.theme.white.opacity(0.025))
     }
 }
