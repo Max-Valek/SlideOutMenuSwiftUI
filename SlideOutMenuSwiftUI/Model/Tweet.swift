d359005a8da3fa5da0bee42b74da3057c8f3b0e7
Max Valek <valek.maxwell@gmail.com>
2023-05-15 11:44:24 -0500

    add basic tweet model

--- SlideOutMenuSwiftUI/Model/Tweet.swift
+++ SlideOutMenuSwiftUI/Model/Tweet.swift
@@ -5,8 +5,15 @@
 //  Created by Max Valek on 5/12/23.
 //
 
-import Foundation
+import SwiftUI
 
 struct Tweet: Identifiable {
     var id = UUID()
+    let author: User
+    let text: String
+    var image: Image?
+    var usersLiked: [User] = []
+    var usersReposted: [User] = []
+    var replies: [Tweet] = []
+    var originalTweetID: UUID?
 }
